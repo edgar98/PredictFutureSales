5258a34b65a1762574ff67454a79e7484d0ed9c1
edgar98 <edgar.ahmadeev@gmail.com>
2021-03-17 00:29:51 +0300

    Linear Regression update

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ import lstm_predictor
 
 if __name__ == '__main__':
     x_train, y_train, x_test, test_data = data_preparation.get_data()
-    lstm_predictor.predict(x_train=x_train, y_train=y_train, x_test=x_test, test_data=test_data)
-    # lin_reg_predictor.predict()
+    # lstm_predictor.predict(x_train, y_train, x_test, test_data)
+    lin_reg_predictor.predict(x_train, y_train, x_test, test_data)
